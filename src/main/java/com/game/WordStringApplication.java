785364ced9cfaaa3f92a180fc10c3486dc28a16c
jose de los santos <delossantosramosjose@gmail.com>
2023-08-28 17:07:05 +0200

    Initial project

---
+++ src/main/java/com/game/WordStringApplication.java
@@ -0,0 +1,13 @@
+package com.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStringApplication.class, args);
+	}
+
+}
